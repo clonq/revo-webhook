7fcfb9e2851e2df591f743962ddd7f8a7aefcb5f
clonq <ovi.comes@gmail.com>
2015-07-12 17:31:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# revo-webhook
+simple webhook component compatible with the revo container
---
+++ component.js
@@ -0,0 +1,35 @@
+module.exports = function(){
+    var _ = require('underscore'),
+    schemaValidator = require('jsonschema'),
+    userSchema = require('bonsens-models').user;
+
+    this.init = function(config) {
+        var self = this;
+        this.params = _.defaults(config||{}, defaults)
+        process.emit('http.route:create', { path:'/webhook/', trigger:'webhook:receive'});
+        process.on('webhook:receive', function(pin){
+            console.log('webhook:receive', pin);
+            var pout = {};
+            var errors = validate(pin);
+            if(errors.length == 0) {
+                //todo add implementtaion here
+            } else {
+                pout.error = { message: errors[0].message };
+                process.emit('webhook:receive.response', pout);
+            }
+        });
+    }
+
+    function validate(payload) {
+        return []; //todo use schema to validate the payload
+    }
+
+}
+
+var defaults = module.exports.defaults = {
+    models: {
+        webhook: {
+            supportedMethods: ['receive']
+        }
+    }
+}

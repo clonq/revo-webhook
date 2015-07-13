3cca664ce7bdfd88c762d912f226da2d057c166b
clonq <ovi.comes@gmail.com>
2015-07-12 21:34:43 -0400

    request processing based on mappings

--- component.js
+++ component.js
@@ -1,19 +1,58 @@
 module.exports = function(){
-    var _ = require('underscore');
+    var _ = require('underscore'),
+        request = require('request');
 
     this.init = function(config) {
         var self = this;
         this.params = _.defaults(config||{}, defaults)
-        process.emit('http.route:create', { path:'/webhook/', trigger:'webhook:receive'});
+        process.emit('http.route:create', { path:'/webhook/*', trigger:'webhook:receive'});
         process.on('webhook:receive', function(pin){
-            console.log('webhook:receive', pin);
             var pout = {};
             var errors = validate(pin);
             if(errors.length == 0) {
-                //todo add implementation here
+                if(!!self.params.mappings) {
+                    self.params.mappings.forEach(function(mapping){
+                        if(!!mapping && !!mapping.in && !!mapping.in.path && (mapping.in.path == pin.path)) {
+                            if(!!mapping.out && !!mapping.out.method) {
+                                if(mapping.out.method == 'get') {
+                                    request.get(mapping.out.url, function(err, res){
+                                        if(err) {
+                                            pout.error = { message: err };
+                                            process.emit('webhook:receive.error', pout);
+                                        } else {
+                                            process.emit('webhook:receive.response', res.body);    
+                                        }
+                                    });
+                                } else if(mapping.out.method == 'post') {
+                                    request.post({url: mapping.out.url, body: JSON.stringify(mapping.out.data)}, function(err, res){
+                                        if(err) {
+                                            pout.error = { message: err };
+                                            process.emit('webhook:receive.error', pout);
+                                        } else {
+                                            process.emit('webhook:receive.response', res.body);    
+                                        }
+                                    });
+                                }
+                            } else {
+                                // default to http get
+                                request.get(mapping.out.url, function(err, res){
+                                    if(err) {
+                                        pout.error = { message: err };
+                                        process.emit('webhook:receive.error', pout);
+                                    } else {
+                                        process.emit('webhook:receive.response', res.body);    
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } else {
+                    pout.error = { message: "no mappings available" };
+                    process.emit('webhook:receive.error', pout);
+                }
             } else {
                 pout.error = { message: errors[0].message };
-                process.emit('webhook:receive.response', pout);
+                process.emit('webhook:receive.error', pout);
             }
         });
     }

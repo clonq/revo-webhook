a7a7d04e45af6b3fdf2424b196f893523497f5e4
clonq <ovi.comes@gmail.com>
2015-07-23 19:12:40 -0400

    using curl for posts...

--- component.js
+++ component.js
@@ -15,6 +15,7 @@ module.exports = function(){
                         if(!!mapping && !!mapping.in && !!mapping.in.path && (mapping.in.path == pin.path)) {
                             if(!!mapping.out && !!mapping.out.method) {
                                 if(mapping.out.method == 'get') {
+                                    console.log('HTTP GET', mapping.out.url);
                                     request.get(mapping.out.url, function(err, res){
                                         if(err) {
                                             pout.error = { message: err };
@@ -24,17 +25,32 @@ module.exports = function(){
                                         }
                                     });
                                 } else if(mapping.out.method == 'post') {
-                                    request.post({url: mapping.out.url, body: JSON.stringify(mapping.out.data)}, function(err, res){
+                                    var curl = 'curl '+mapping.out.url+' -d \''+JSON.stringify(mapping.out.data)+'\'';
+                                    require('child_process').exec(curl, function (err, stdout, stderr) {
                                         if(err) {
-                                            pout.error = { message: err };
                                             process.emit('webhook:receive.error', pout);
                                         } else {
-                                            process.emit('webhook:receive.response', res.body);    
+                                            process.emit('webhook:receive.response', {out: stdout, err: stderr})
                                         }
                                     });
+                                    // todo: investigate why request.post is not working
+                                    // request.post({url: mapping.out.url, body: JSON.stringify(mapping.out.data)}, function(err, res){
+                                    // request.post({url: mapping.out.url, json: mapping.out.data}, function(err, res){
+                                    //     if(err) {
+                                    //         console.log('ERROR:', err);
+                                    //         pout.error = { message: err };
+                                    //         process.emit('webhook:receive.error', pout);
+                                    //     } else {
+                                    //         console.log('response.statusCode:', res.statusCode);
+                                    //         console.log('res.headers:', res.headers);
+                                    //         console.log('res.body:', res.body);
+                                    //         process.emit('webhook:receive.response', res.body);    
+                                    //     }
+                                    // });
                                 }
                             } else {
                                 // default to http get
+                                console.log('HTTP GET', mapping.out.url);
                                 request.get(mapping.out.url, function(err, res){
                                     if(err) {
                                         pout.error = { message: err };
